b6b8c005e2ab0fa6e49295786337f20d802f639b
ensallee <ensallee@gmail.com>
2018-08-14 15:46:29 -0400

    capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,37 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// MY SOLUTION
+// function capitalize(str) {
+//   return str.split(" ").map(word => {
+//     let cap = word[0].toUpperCase()
+//     return cap += word.slice(1)
+//   }).join(" ")
+// }
+
+//GRIDERS SOLUTION
+// function capitalize(str) {
+//   const words = []
+//
+//   for (let word of str.split(' ')) {
+//     words.push(word[0].toUpperCase() + word.slice(1))
+//   }
+//
+//   return words.join(' ');
+// }
+
+// GRIDERS SECOND SOLUTION
+function capitalize(str) {
+  let result = str[0].toUpperCase()
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i -1] === ' ') {
+      result += str[i].toUpperCase()
+    } else {
+      result += str[i]
+    }
+  }
+  return result
+}
 
 module.exports = capitalize;
